8afecbcaaaa2d1fe72f7500581dac1b797d604ef
Noway <ahmadrezamohammadi777@gmail.com>
2023-11-13 13:15:38 +0330

    Now todos are dynamic and delete all button added!

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ const taskInput = document.getElementById('task-input');
 const dateInput = document.getElementById('task-date');
 const addButton = document.getElementById('add-button');
 const alertMsg = document.getElementById('alert-msg');
+const todosBody = document.querySelector('tbody')
 const allButton = document.getElementById('all-btn');
 const pendingButton = document.getElementById('pending-btn');
 const completedButton = document.getElementById('completed-btn');
@@ -15,34 +16,56 @@ generateId()
 
 
 
-const todos = JSON.parse(localStorage.getItem('todos')) || [];
+let todos = JSON.parse(localStorage.getItem('todos')) || [];
 console.log(todos);
 
 const saveToLocalStorage = () => {
-    localStorage.setItem('todos', JSON.stringify(todos));
+    localStorage.setItem('todos', JSON.stringify(todos))
+};
 
 
 const showAlert = (message, type) => {
-    alertMsg.innerHTML = ''
+    alertMsg.innerHTML = '';
     const alert = document.createElement('p');
     alert.innerText = message;
     if (type === 'error') {
-        alert.classList.add('bg-danger')
-        alert.classList.add('rounded-2')
+        alert.classList.add('bg-danger');
+        alert.classList.add('rounded-2');
     } else {
-        alert.classList.add('bg-success')
-        alert.classList.add('rounded-2')
+        alert.classList.add('bg-success');
+        alert.classList.add('rounded-2');
     }
-    alertMsg.append(alert)
+    alertMsg.append(alert);
     setTimeout(() => {
         alert.style.display = 'none';
-    }, 2000)
-
-}
-
-
-
+        alertMsg.removeChild(alert);
+    }, 2000);
 };
+
+const displayTodos = () => {
+    todosBody.innerHTML = '';
+
+    if (!todos.length) {
+        todosBody.innerHTML = '<tr><td colspan="4">No task found!</td></tr>';
+        return;
+    }
+    todos.forEach((todo) => {
+        todosBody.innerHTML += `
+        <tr>
+        <td>${todo.task}</td>
+        <td>${todo.date || 'No date'}</td>
+        <td>${todo.completed ? 'Completed' : 'Pending'}</td>
+        <td>
+        <button class = 'btn btn-warning mx-1'>edit</button>
+        <button class = 'btn btn-success mx-1'>do</button>
+        <button class = 'btn btn-danger mx-1'>delete</button>
+        </td>
+        </tr>
+        `
+    })
+}
+displayTodos();
+
 
 const addHandler = () => {
     const task = taskInput.value;
@@ -51,12 +74,13 @@ const addHandler = () => {
         id: generateId(),
         completed: false,
         task: task,
-        data: date,
+        date: date,
 
     };
     if (task) {
         todos.push(todo);
-        saveToLocalStorage();   
+        saveToLocalStorage();
+        displayTodos();
         taskInput.value = '';
         dateInput.value = '';
         console.log(todos);
@@ -66,6 +90,22 @@ const addHandler = () => {
     }
 
 }
+
+const deleteAllHandler = () => {
+    if (todos.length) {
+        todos = [];
+        saveToLocalStorage();
+        displayTodos();
+        showAlert('All todos has been deleted successfully', 'success');
+
+    }else{
+        showAlert('No todos to clear!', 'error')
+    }
+
+};
+
+
 addButton.addEventListener('click', addHandler);
+window.addEventListener('load', displayTodos)
 
-
+deleteAllButton.addEventListener('click', deleteAllHandler);
